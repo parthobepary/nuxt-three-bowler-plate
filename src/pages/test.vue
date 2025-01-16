eece249d799316f2ecf6ba8df8f66281b86191c7
partho <pallobbiswas326@gmail.com>
2025-01-16 11:15:54 +0600

    fgfgfgf~~

---
+++ src/pages/test.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const x = 1;
+const y = 2
+
+const sum = ref(0);
+
+watchEffect(() => {
+  sum.value = x + y;
+});
+</script>
+
+<template>
+  <div>
+
+    {{ sum }}
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus architecto at culpa distinctio dolorum eaque
+      et, illum in laboriosam magnam molestias
+      necessitatibus nemo nihil nisi provident quidem quos, ut veniam!
+    </p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
